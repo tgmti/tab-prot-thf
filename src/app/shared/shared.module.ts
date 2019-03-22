23ee30b348902567f06e8a615f60e4f0c148f12b
jhonyeduardo <jhony.senem@totvs.com>
2019-03-22 15:16:14 -0300

    DTHFUI-103 - added FormsModule in sharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { ThfModule } from '@totvs/thf-ui';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     ThfModule
   ],
   exports: [
     CommonModule,
+    FormsModule,
     ThfModule
   ]
 })
