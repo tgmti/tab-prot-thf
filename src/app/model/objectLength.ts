0275f74bd4ba7e49faf189c2165fb1d58c605360
TGM - Thiago Mota <tgmspawn@gmail.com>
2019-08-07 19:20:48 -0300

    Limpeza do projeto antigo

--- src/app/model/objectLength.ts
+++
@@ -1,6 +0,0 @@
-export interface ObjectLength {
-    /**
-     * Length
-     */
-    length: number;
-  }
--- src/app/model/param.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface Param {
-    id?: string;
-    x6_fil: string;
-    x6_var: string;
-    x6_conteud: string; 
-    x6_tipo: string; 
-    x6_descric: string; 
-    x6_propri: string;
-  }
