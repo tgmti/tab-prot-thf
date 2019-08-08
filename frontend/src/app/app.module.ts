c0c170db08766fb5517caeed208b7cc5de074d94
TGM - Thiago Mota <tgmspawn@gmail.com>
2019-08-08 00:25:34 -0300

    Init Frontend with PortinariUI + THF Theme
    add @totvs/portinari-theme
    add portinari tslint
    add portinariui
    Config App Menu
    Config init pages

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PoModule } from '@portinari/portinari-ui';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PoModule,
+    RouterModule.forRoot([])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
