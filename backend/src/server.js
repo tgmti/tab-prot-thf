46e272e8f6d553033e0dbf3267e57d5e3502e5cc
TGM - Thiago Mota <tgmspawn@gmail.com>
2019-08-08 02:45:18 -0300

    Enable Cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const cors = require('cors');
 const routes = require('./routes');
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
