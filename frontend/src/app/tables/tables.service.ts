81d2d909d10563fc11d1025f911e8f4ae33b34ea
TGM - Thiago Mota <tgmspawn@gmail.com>
2019-08-11 17:28:53 -0300

    Legendas e Cores na tela de Tabelas

--- frontend/src/app/tables/tables.service.ts
+++ frontend/src/app/tables/tables.service.ts
@@ -17,13 +17,21 @@ export class TablesService extends HttpService {
 
     this.literals = {...this.literals, ...literalService.tables};
 
+    const shareTypeColumnTemplate = {
+      width: '80px',
+      type: 'subtitle', subtitles: [
+        { value: 'E', color: 'color-08', label: 'Exclusivo', content: 'E' },
+        { value: 'C', color: 'color-11', label: 'Compartilhado', content: 'C' },
+      ]
+    };
+
     this.columns = [
-      { property: 'x2_chave', label: this.literals['x2_chave'] },
-      { property: 'x2_nome', label: this.literals['x2_nome'] },
-      { property: 'x2_modo', label: this.literals['x2_modo'] },
-      { property: 'x2_modoun', label: this.literals['x2_modoun'] },
-      { property: 'x2_modoemp', label: this.literals['x2_modoemp'] },
-      { property: 'x2_unico', label: this.literals['x2_unico'] },
+      { property: 'x2_chave', label: this.literals['x2_chave'], width: '80px' },
+      { property: 'x2_nome', label: this.literals['x2_nome'], width: '200px' },
+      { property: 'x2_modo', label: this.literals['x2_modo'] , ...shareTypeColumnTemplate },
+      { property: 'x2_modoun', label: this.literals['x2_modoun'] , ...shareTypeColumnTemplate },
+      { property: 'x2_modoemp', label: this.literals['x2_modoemp'] , ...shareTypeColumnTemplate },
+      { property: 'x2_unico', label: this.literals['x2_unico'], width: '200px' },
     ];
 
   }
