c2d8af20b5938dcac30400fd6bd418e076d6ee3f
TGM - Thiago Mota <tgmspawn@gmail.com>
2019-08-12 01:35:09 -0300

    Criação dos DockerFile para Backend, Frontend e Database

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 
 const configdb = {
     database: 'protheus',
-    host: '127.0.0.1',
+    host: '172.17.0.2',
     user: 'postgres',
     password: 'Totvs@123',
     port: 5432,
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url_api: '',
+  url_api: '172.17.0.3',
 };
