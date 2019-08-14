5bc667d85a3166892d93f572597397e0d266271f
TGM - Thiago Mota <tgmspawn@gmail.com>
2019-08-14 03:33:35 -0300

    Imagem do banco vai ficar para outro momento

--- database/PostgreSQL/cleandatabase.sql
+++
@@ -1,7 +0,0 @@
-DO $$ DECLARE
-    r RECORD;
-BEGIN
-    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema() and tablename not in ('sx2990','sx3990','six990','sx6990')) LOOP
-        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-    END LOOP;
-END $$;
