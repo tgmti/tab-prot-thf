46a493436d3b7b9cde76570e4a99857148f0f770
TGM <tgmspawn@gmail.com>
2020-02-18 02:36:59 -0300

    Update Portinari e Angular (#6)
    * Portinari UI atualizado para 1.26.0
    
    * Angular atualizado para v9.0.1
    * Desabilitado Ivy do Angular, pois não funcionou com Portinari
    * Imagem Docker atualizada em https://hub.docker.com/r/tgmti/tabelas-protheus
      * Tags da imagem: latest, 2.0.0, 2.0.0-angular9
    * Arrumado link da home no menu

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { PoMenuItem } from '@portinari/portinari-ui';
 export class AppComponent {
 
   readonly menus: Array<PoMenuItem> = [
-    { label: 'Home', action: this.onClick.bind(this) },
+    { label: 'Home', link: '/' },
     { label: 'Tabelas', link: '/tables' },
     { label: 'Campos', link: '/fields' },
     { label: 'Indices', link: '/indexes' },
