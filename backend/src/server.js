bfb9cf52f592c7b8b5e5eaa0d559bbe2f824dff5
Thiago Mota <tgmspawn@gmail.com>
2020-06-19 09:50:18 -0300

    Express.urlencoded substituido por bodyParser.urlencoded

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const bodyParser = require('body-parser');
 const APIPORT = 3333
 const server = express();
 
 server.use(cors());
 server.use(express.json());
-server.use(express.urlencoded());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 server.use(routes);
 
 server.listen(APIPORT, ()=> console.info(`API Listening on ${APIPORT} port...`));
